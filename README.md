d7f93fe9fa91122bb6ef61b512e4d224ec7ce529
meghanashastri1 <meghana360@gmail.com>
2020-05-22 22:34:21 -0700

    README.md

--- README.md
+++ README.md
@@ -1,1 +1,12 @@
 # mongodb_tutorial
+
+Meghana Shastri
+CPSC 408
+2317343
+
+Instructions: run python3 mongodb.py
+
+References:
+- https://docs.mongodb.com/manual/tutorial/getting-started/
+- https://www.w3schools.com/python/python_mongodb_getstarted.asp
+
---
+++ mongodb.py
@@ -0,0 +1,19 @@
+import pymongo
+import dns
+
+
+client = pymongo.MongoClient("mongodb+srv://<root1>:<mongodb>@cluster0-sfquv.gcp.mongodb.net/test?retryWrites=true&w=majority")
+db = client.test
+
+col = db["inventory"]
+
+#populate collection
+db.inventory.insertMany([
+   { item: "journal", qty: 25, status: "A", size: { h: 14, w: 21, uom: "cm" }, tags: [ "blank", "red" ] },
+   { item: "notebook", qty: 50, status: "A", size: { h: 8.5, w: 11, uom: "in" }, tags: [ "red", "blank" ] },
+   { item: "paper", qty: 10, status: "D", size: { h: 8.5, w: 11, uom: "in" }, tags: [ "red", "blank", "plain" ] },
+   { item: "planner", qty: 0, status: "D", size: { h: 22.85, w: 30, uom: "cm" }, tags: [ "blank", "red" ] },
+   { item: "postcard", qty: 45, status: "A", size: { h: 10, w: 15.25, uom: "cm" }, tags: [ "blue" ] }
+]);
+
+
